9f6573434fbce044d65356e6588a5e84377398fe
happycoder29 <ashish@drillmaps.com>
2019-10-17 19:06:14 -0700

    Adding realm DB and getting file path

--- StockApp/TableViewController.swift
+++ StockApp/TableViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import RealmSwift
+import SwiftyJSON
+import Alamofire
 
 class TableViewController: UITableViewController {
 
+    var arr = [Stock]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do{
+            let realm = try Realm()
+
+        }catch{
+            print("Error in initialiozing \(error)")
+        }
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+                
+        
+        
 
     }
 
@@ -19,15 +36,17 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return arr.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as!  TableViewCell
+        
+        cell.lblSymbol.text = "\(arr[indexPath.row].symbol) : "
+        cell.lblPrice.text = "\(arr[indexPath.row].price) $"
+        
         return cell
     }
     
